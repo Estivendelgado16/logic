f92275db263f57953f648b4aca531c075146c6ba
Estivendelgado16 <brallamdelgado351@gmail.com>
2026-07-06 12:01:17 -0500

    for cicle

---
+++ for/dosAlveinte.js
@@ -0,0 +1,7 @@
+//Problema 2
+
+//Imprime únicamente los números pares del 2 al 20.
+
+for (let i = 2; i <= 20; i+= 2) {
+    console.log(i)
+}
---
+++ for/unoAlCien.js
@@ -0,0 +1,6 @@
+//Problema 1
+//Imprime los números del 1 al 10.
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+};
